13d5ae219c2933f0727f126fd7eb5085674a9238
Alef <kaiopinto738@gmail.com>
2025-09-15 10:28:13 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/else.py
@@ -0,0 +1,7 @@
+nome_input = str(input("Digite seu nome: "))
+nome_correto = "Alef"
+
+if nome_input == nome_correto:
+    print("Correto!")
+else:
+    print("Nome errado!")
---
+++ Modulo 1/Condicionais/festa.py
@@ -0,0 +1,9 @@
+lavou = input("Lavou a louça? 1-sim | 2-não: ")
+guardou = input("Guardou a louça? 1-sim | 2-não")
+
+if lavou == "sim" and guardou == "sim":
+    print("Pode ir pra festa🎉🎉🎉🎉🎉🎉🎉")
+elif lavou == "sim":
+    print("Falta guardar a louça")
+else:
+    print("Não pode 😔😔😔😔😔")
